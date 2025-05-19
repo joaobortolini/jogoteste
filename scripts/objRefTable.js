b931486d82d99fb65245292fc0c5e5b5f76e2c7e
joaobortolini <j.bortolini@escola.pr.gov.br>
2025-05-19 08:48:02 -0300

    Add files via upload

---
+++ scripts/objRefTable.js
@@ -0,0 +1,70 @@
+const C3 = self.C3;
+self.C3_GetObjectRefTable = function () {
+	return [
+		C3.Plugins.Sprite,
+		C3.Behaviors.solid,
+		C3.Behaviors.Platform,
+		C3.Behaviors.bound,
+		C3.Plugins.Text,
+		C3.Plugins.Button,
+		C3.Plugins.Sprite.Cnds.OnCollision,
+		C3.Plugins.System.Acts.RestartLayout,
+		C3.Plugins.System.Acts.SetVar,
+		C3.Plugins.System.Cnds.EveryTick,
+		C3.Plugins.System.Acts.Scroll,
+		C3.Plugins.Sprite.Exps.X,
+		C3.Plugins.Sprite.Exps.Y,
+		C3.Plugins.Text.Acts.SetText,
+		C3.Plugins.Sprite.Acts.Destroy,
+		C3.Plugins.System.Acts.AddVar,
+		C3.Plugins.Text.Acts.SetVisible,
+		C3.Plugins.System.Cnds.Compare,
+		C3.Behaviors.solid.Acts.SetEnabled,
+		C3.Plugins.Sprite.Acts.SetAnim,
+		C3.Plugins.System.Acts.GoToLayout,
+		C3.Plugins.Button.Cnds.OnClicked
+	];
+};
+self.C3_JsPropNameTable = [
+	{Sólido: 0},
+	{Sprite: 0},
+	{Plataforma: 0},
+	{RestritoAoLayout: 0},
+	{Sprite2: 0},
+	{Sprite3: 0},
+	{Sprite4: 0},
+	{Sprite5: 0},
+	{Sprite6: 0},
+	{Texto: 0},
+	{Sprite7: 0},
+	{Sprite10: 0},
+	{Texto2: 0},
+	{Sprite9: 0},
+	{Sprite8: 0},
+	{Sprite11: 0},
+	{Texto3: 0},
+	{Sprite13: 0},
+	{Sprite14: 0},
+	{Botão: 0},
+	{pontos: 0}
+];
+
+self.InstanceType = {
+	Sprite: class extends self.ISpriteInstance {},
+	Sprite2: class extends self.ISpriteInstance {},
+	Sprite3: class extends self.ISpriteInstance {},
+	Sprite4: class extends self.ISpriteInstance {},
+	Sprite5: class extends self.ISpriteInstance {},
+	Sprite6: class extends self.ISpriteInstance {},
+	Texto: class extends self.ITextInstance {},
+	Sprite7: class extends self.ISpriteInstance {},
+	Sprite10: class extends self.ISpriteInstance {},
+	Texto2: class extends self.ITextInstance {},
+	Sprite9: class extends self.ISpriteInstance {},
+	Sprite8: class extends self.ISpriteInstance {},
+	Sprite11: class extends self.ISpriteInstance {},
+	Texto3: class extends self.ITextInstance {},
+	Sprite13: class extends self.ISpriteInstance {},
+	Sprite14: class extends self.ISpriteInstance {},
+	Botão: class extends self.IButtonInstance {}
+}
